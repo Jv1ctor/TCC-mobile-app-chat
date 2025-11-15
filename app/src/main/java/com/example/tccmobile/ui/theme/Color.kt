8feab8e27e565249166b0636ecf898cde9f77070
Rebeca Sena <r3b3casena@gmail.com>
2025-11-14 21:50:32 -0300

    adicionei mais cores

--- app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
@@ -8,7 +8,17 @@ val Pink80 = Color(0xFF062452)
 
 val BlueEscuro = Color(0xFF062452)
 
+val CianoAzul = Color(0xFF0282EA)
 
+val Branco = Color(0xFFFFFFFF)
+
+val AzulClaro = Color(0xFF2287FF)
+
+val AzulLetra = Color(0xFF003366)
+
+val AzulSuperClaro = Color(0xFFC0E0FF)
+
+val Cinza = Color(0xFF64748B)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
