5a5431a9ee7b8e7c7f1bb99d39adff2623342c5d
Reb Sena <r3b3casena@gmail.com>
2025-11-20 01:05:24 -0300

    Update color definitions in Color.kt

--- app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
@@ -4,7 +4,51 @@ import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink80 = Color(0xFF062452)
+
+val BlueEscuro = Color(0xFF062452)
+
+val CianoAzul = Color(0xFF0282EA)
+
+val Branco = Color(0xFFFFFFFF)
+
+val AzulClaro = Color(0xFF2287FF)
+
+val AzulLetra = Color(0xFF003366)
+
+val AzulSuperClaro = Color(0xFFC0E0FF)
+
+val Cinza = Color(0xFF64748B)
+
+val VermelhoTelha = Color(0xFF973C00)
+
+val AmareloClaro1 = Color(0xFFFEE685)
+
+val AmareloClaro2 = Color(0xFFFEF3C6)
+
+val Laranja = Color(0xFFFFD6A7)
+
+val Azul2 = Color(0xFFBEDBFF)
+
+val Cinza2 = Color(0xFFE5E7EB)
+
+val Cinza3 = Color(0xFFF3F4F6)
+
+val AzulNoite = Color(0xFF1B5C9D)
+
+val CinzaAzul = Color(0xFFD7EDFF)
+
+val Black = Color(0xFF1A1A1A)
+
+val AmareloEstrela = Color(0xFFF0B100)
+
+val Rosinha = Color(0xFFFFC0C2)
+
+val VerdeEscuro = Color(0xFF016630)
+
+val Vermelho = Color(0xFFF54900)
+
+val LaranjaMedalha = Color(0xFFD08700)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
