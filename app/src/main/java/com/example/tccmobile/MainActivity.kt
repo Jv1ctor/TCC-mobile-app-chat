c25f1a22fc0bbacc78bf546d56829c7001b816cc
Reb Sena <r3b3casena@gmail.com>
2025-11-19 21:12:00 -0300

    Add files via upload

--- app/src/main/java/com/example/tccmobile/MainActivity.kt
+++ app/src/main/java/com/example/tccmobile/MainActivity.kt
@@ -11,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tccmobile.Components.Teste
+
 import com.example.tccmobile.ui.theme.TCCMobileTheme
+import com.example.tccmobile.TicketApp.TicketApp
+
+import com.example.tccmobile.TicketApp.TicketApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TCCMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TicketApp()  // 👈 Chama a tela normalmente
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TCCMobileTheme {
-        Greeting("Android")
     }
 }
