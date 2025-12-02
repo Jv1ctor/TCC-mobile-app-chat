cdd87793efe68ac787c4095aae3146c5b70f1c5f
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-12-01 23:47:35 -0300

    feat: add entity message

--- app/src/main/java/com/example/tccmobile/data/entity/Message.kt
+++ app/src/main/java/com/example/tccmobile/data/entity/Message.kt
@@ -1,3 +1,17 @@
 package com.example.tccmobile.data.entity
 
-data class Message()
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+data class Message @OptIn(ExperimentalTime::class) constructor(
+    val id: Int,
+    val content: String,
+    val senderName: String,
+    val ticketId: String,
+    val createdAt: Instant,
+    val isStudent: Boolean,
+    val fileUrl: String? = null,
+    val fileName: String? = null,
+    val fileSize: Int? = null,
+    val fileType: String? = null,
+)
