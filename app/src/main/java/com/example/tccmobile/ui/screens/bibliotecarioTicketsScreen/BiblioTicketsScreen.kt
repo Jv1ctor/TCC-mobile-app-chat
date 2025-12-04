457e0de129315d0aa0c6c195d9491bd983e6a901
Breno <brenosg2310@gmail.com>
2025-12-04 19:16:35 -0300

    feat: Adicionando tela de tickets bibliotecario

---
+++ app/src/main/java/com/example/tccmobile/ui/screens/bibliotecarioTicketsScreen/BiblioTicketsScreen.kt
@@ -0,0 +1,14 @@
+package com.example.tccmobile.ui.screens.bibliotecarioTicketsScreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.tccmobile.ui.components.bibliotecaria.HeaderTickets
+
+@Composable
+fun BiblioTicketsScreen() {
+    Column {
+//        HeaderTickets(
+//
+//        )
+    }
+}
