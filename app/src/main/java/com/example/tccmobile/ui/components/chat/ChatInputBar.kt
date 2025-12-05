6f235181719c8541788b9974727e1f2df07d08c8
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-12-05 03:49:08 -0300

    feat: add visualizer attachment

--- app/src/main/java/com/example/tccmobile/ui/components/chat/ChatInputBar.kt
+++ app/src/main/java/com/example/tccmobile/ui/components/chat/ChatInputBar.kt
@@ -1,11 +1,15 @@
 package com.example.tccmobile.ui.components.chat
 
+import android.text.Layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.outlined.AttachFile
+import androidx.compose.material.icons.outlined.Description
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -16,23 +20,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tccmobile.ui.theme.Blue
 import com.example.tccmobile.ui.theme.DarkBlue
 import com.example.tccmobile.ui.theme.LightBlue
+import com.example.tccmobile.ui.theme.White
 
 @Composable
 fun ChatInputBar(
     message: String,
+    fileName: String,
     onMessageChange: (String) -> Unit, // função que avisa que usuário digitou uma algo novo
     onSendClick: () -> Unit,        // ação quando clica no aviãozinho
-    onAttachClick: () -> Unit       // ação quando clica no clipe
+    onAttachClick: () -> Unit,       // ação quando clica no clipe
+    onRemoveFile: () -> Unit
 ) {
-
-
-
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -41,14 +48,63 @@ fun ChatInputBar(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        if(fileName.isNotBlank()){
+            Box(
+                modifier = Modifier
+                    .padding(12.dp) // margem externa
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Blue)
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Description,
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        tint = White
+                    )
+
+                    Text(
+                        text = fileName,
+                        fontSize = 16.sp,
+                        color = White,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier
+                            .weight(1f) // ocupa o espaço para empurrar o X para a direita
+                            .padding(horizontal = 12.dp, vertical = 6.dp)
+                    )
+
+                    IconButton(
+                        onClick = { onRemoveFile() },
+                        modifier = Modifier.size(24.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Remover",
+                            tint = White,
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            }
+
+        }
+
+
+
+
 
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-
-
             IconButton(// botão de anexo
                 onClick = onAttachClick,
                 modifier = Modifier
@@ -125,6 +181,9 @@ fun ChatInputPreview() {
         message = "",
         onMessageChange = {},
         onSendClick = {},
-        onAttachClick = {}
+        fileName = "teste.doc",
+        onAttachClick = {},
+        onRemoveFile = {}
+
     )
 }
