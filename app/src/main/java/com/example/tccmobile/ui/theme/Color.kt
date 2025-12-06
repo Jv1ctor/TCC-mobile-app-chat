536be0a3fc58b7468609770d281736800998d512
Rebeca Sena <r3b3casena@gmail.com>
2025-12-06 12:59:43 -0300

    Cores comentadas com seus respectivos lugares e utilidades

--- app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/tccmobile/ui/theme/Color.kt
@@ -1,80 +1,49 @@
 package com.example.tccmobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
-val ClaroBlueBackground = Color(0xFFB8D1FF)
-val HeaderBackground = Color(0xFF204879)
-val Purple80 = Color(0xFFD0BCFF)
-val containerBlue = Color(0xFF003366)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFF062452)
+val ClaroBlueBackground = Color(0xFFB8D1FF) //usado em metriccardrow e dashboardviewmodel
+val HeaderBackground = Color(0xFF204879) //usado em recentreviewcards
+val Purple80 = Color(0xFFD0BCFF) //usado no Theme como cor primary
+val containerBlue = Color(0xFF003366) //usado no Header (NÃO É PRA MUDAR >:( )
+val PurpleGrey80 = Color(0xFFCCC2DC) //usado no Theme como cor secondary
+val Pink80 = Color(0xFF062452) //usado no Theme como cor tetiary
 
-val BlueEscuro = Color(0xFF062452)
+val BlueEscuro = Color(0xFF062452) //usado no new Ticket form e header
 
-val CianoAzul = Color(0xFF0282EA)
+val CianoAzul = Color(0xFF0282EA) //usado no new ticket form
 
-val Branco = Color(0xFFFFFFFF)
+val Branco = Color(0xFFFFFFFF) //usado em 11 cantos diferentes
 
-val AzulClaro = Color(0xFF2287FF)
-val AzulLetra = Color(0xFF003366)
-val AzulSuperClaro = Color(0xFFC0E0FF)
+val AzulLetra = Color(0xFF003366) //usado no new Ticket form e header
+val AzulSuperClaro = Color(0xFFC0E0FF) //usado no new ticket header
 
-val Cinza = Color(0xFF64748B)
+val Cinza = Color(0xFF64748B) //usado no new ticket form
 
-val VermelhoTelha = Color(0xFF973C00)
-
-val AmareloClaro1 = Color(0xFFFEE685)
-
-val Laranja = Color(0xFFFFD6A7)
-
-val Azul2 = Color(0xFFBEDBFF)
-
-val Cinza2 = Color(0xFFE5E7EB)
-
-val Cinza3 = Color(0xFFF3F4F6)
-
-val AzulNoite = Color(0xFF1B5C9D)
-
-val CinzaAzul = Color(0xFFD7EDFF)
-
-val Black = Color(0xFF1A1A1A)
-
-val AmareloEstrela = Color(0xFFF0B100)
-
-val Rosinha = Color(0xFFFFC0C2)
-
-val VerdeEscuro = Color(0xFF016630)
-
-val Vermelho = Color(0xFFF54900)
-
-val LaranjaMedalha = Color(0xFFD08700)
-
-val Purple40 = Color(0xFF6650a4)
-val BackgroundIconTime = Color(0xFFD3CDCD)
-val Pink40 = Color(0xFFFF004F)
-
-val white = Color(0xFFFFFFFF)
+val VermelhoTelha = Color(0xFF973C00) //usado para o relógio pendentes
 
 
+val CinzaAzul = Color(0xFFD7EDFF) //usado no new Ticket form
 
-val yellow = Color(0xFFF6E7B4)
-val green = Color(0xFF00A63E)
-val AmareloClaro2 = Color(0xFFFFF4C2)
-val IconBackgroundGreen = Color(0xFFD2F3B0)
+val Black = Color(0xFF1A1A1A) //usado em ticket form
+val BackgroundIconTime = Color(0xFFD3CDCD) //usado em Dashboardviewmodel
+val Pink40 = Color(0xFFFF004F) //usado no Theme como cor secundary
 
-val IconBackgroundYellow = Color(0xFFFFF5D0)
-val IconBackgroundRed = Color(0xFFF8D7DA)
-val yellow2 = Color(0xFFFFC107)
-val incrementosgreen = Color(0xFF2ECC71)
-val BadgePositionColor = Color(0xFFAAAAAA)
-val BadgePositionColor2 = Color(0xFFCE8034)
-val BadgePositionColor3 = Color(0xFF566F7C)
-val PurpleGrey40 = Color(0xFF625b71)
-val Bluebutton = Color(0xFF006CFF)
 
+val green = Color(0xFF00A63E) //usado no icone concluídos
+val AmareloClaro2 = Color(0xFFFFF4C2) //fundo do icone Avaliação Média
+val IconBackgroundGreen = Color(0xFFD2F3B0) //usado em Dashboardviewmodel e metriccardrow
+val IconBackgroundRed = Color(0xFFF8D7DA) //usado em Dashboardviewmodel e metriccardrow - pendentes
+val yellow2 = Color(0xFFFFC107) //usado para as estrelas e icone 1° lugar
+val incrementosgreen = Color(0xFF2ECC71) //verde usado para +12 este mês
+val BadgePositionColor = Color(0xFFAAAAAA) //usado em Teamperformacecard
+val BadgePositionColor2 = Color(0xFFCE8034) //usado em Teamperformacecard
+val BadgePositionColor3 = Color(0xFF566F7C) //usado em Teamperformacecard
+val PurpleGrey40 = Color(0xFF625b71) //fundo do icone Tempo médio de Resposta
+val Bluebutton = Color(0xFF006CFF) //usado em button form
 val DarkBlueBackground = Color(0xFF062452) // Fundo do BoxForm
 val PrimaryBlue = Color(0xFF0282EA)       // Botão "Entrar"
 val DarkBlueButton = Color(0xFF062452)    // Botão de toggle ativo
-val TextPrimary = Color.White
+val TextPrimary = Color.White //usado em loginScreen e Registerscreen
 val TextSecondary = Color(0xFFB0B0B0)      // Placeholder/Helper
 val BackgroundStart = Color(0xFF2287FF)    // Início do Gradiente
 val BackgroundEnd = Color(0xFF034384)      // Fim do Gradiente
