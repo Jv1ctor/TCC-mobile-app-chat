c9110f5b9ed16b27785657ed54c23bc607432360
Breno <brenosg2310@gmail.com>
2025-12-05 21:24:20 -0300

    feat: Adicionado rota de tela de tickets bibliotecaria

--- app/src/main/java/com/example/tccmobile/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/tccmobile/navigation/AppNavigation.kt
@@ -1,14 +1,16 @@
 package com.example.tccmobile.navigation
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Dashboard
 import androidx.compose.material.icons.outlined.Description
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tccmobile.ui.components.utils.BottomNavItem
-import com.example.tccmobile.ui.components.utils.BottomNavigationBar
+import com.example.tccmobile.ui.screens.bibliotecarioTicketsScreen.BiblioTicketsScreen
 import com.example.tccmobile.ui.screens.loginScreen.LoginScreen
 import com.example.tccmobile.ui.screens.registerScreen.RegisterScreen
 import com.example.tccmobile.ui.screens.studentTicketsScreen.DashboardTicketsScreen
@@ -50,7 +52,7 @@ import com.example.tccmobile.ui.screens.studentTicketsScreen.DashboardTicketsScr
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Routes.HOME) {
+    NavHost(navController = navController, startDestination = Routes.BIBLIO_TICKETS) {
 
         composable(Routes.LOGIN) {
             LoginScreen(
@@ -101,6 +103,46 @@ fun AppNavigation() {
                     println("Cliquei no ticket: $ticketId")
                 }
             )
+        }
+        composable(Routes.BIBLIO_TICKETS) {
+            BiblioTicketsScreen(
+                navigateBarItems = listOf(
+                    BottomNavItem(
+                        label = "Tickets",
+                        icon = Icons.Outlined.Description,
+                        route = Routes.BIBLIO_TICKETS,
+                        onClick = { route ->
+                            navController.navigate(route)
+                        }
+                    ),
+                    BottomNavItem(
+                        label = "Dashboard",
+                        icon = Icons.Outlined.Dashboard,
+                        route = "dashboard_route", // Defina a rota correta para o dashboard
+                        onClick = { route ->
+                            navController.navigate(route)
+                        }
+                    ),
+                    BottomNavItem(
+                        label = "Perfil",
+                        icon = Icons.Outlined.Person,
+                        route = Routes.PROFILE,
+                        onClick = { route ->
+                            navController.navigate(route)
+                        }
+                    )
+                ),
+                currentRoute = Routes.BIBLIO_TICKETS,
+                onTicketClick = { ticketId ->
+                    println("Cliquei no ticket: $ticketId")
+                },
+                onDashboardClick = {
+                    navController.navigate(Routes.BIBLIO_DASHBOARD)
+                }
+            )
+        }
+        composable(Routes.BIBLIO_DASHBOARD) {
+            Text(text = "Dashboard Screen")
         }
     }
 }
