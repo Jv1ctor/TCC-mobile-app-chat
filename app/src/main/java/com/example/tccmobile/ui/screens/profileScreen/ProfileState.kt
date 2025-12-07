2db3c9f5a9be2ee6e5f26099214d0479134f8365
Kauam <kauam.morais09@gmail.com>
2025-12-07 11:42:35 -0300

    fix: Removed permissions, and fix variables

--- app/src/main/java/com/example/tccmobile/ui/screens/profileScreen/ProfileState.kt
+++ app/src/main/java/com/example/tccmobile/ui/screens/profileScreen/ProfileState.kt
@@ -10,39 +10,12 @@ data class Student(
     val tccsEmAnalise: Int = 0
 )
 
-enum class AppPermission(val displayName: String, val isSpecial: Boolean = false) {
-    MANAGE_TICKETS("Gerenciar Tickets"),
-    TRANSFER_TICKETS("Transferir Tickets"),
-    FINALIZE_TICKETS("Finalizar Tickets"),
-    // O ADMINISTRADOR é marcado como especial
-    ADMINISTRATOR("Administrador", isSpecial = true);
-}
-
-data class PermissionItem(
-    val nome: String,
-    val hasPermission: Boolean,
-    val isSpecial: Boolean
-)
-fun generatePermissions(permissionsGranted: Set<AppPermission>): List<PermissionItem> {
-    return AppPermission.entries.map { appPermission ->
-        PermissionItem(
-            nome = appPermission.displayName,
-            hasPermission = permissionsGranted.contains(appPermission),
-            isSpecial = appPermission.isSpecial
-        )
-    }
-}
-
 data class Librarian(
     val nameLibrarian: String,
     val role: String, // Cargo ou Função
     val emailLibrarian: String,
     val phoneLibrarian: String = "", // Opcional
-    val corrigidos: Int = 0,
-    val pendentes: Int = 0,
-    val avaliacaoMedia: Float = 0f,
-    val tempoMedio: String = "",
-    val permissoes: List<PermissionItem> = emptyList()
+    val recebidos: Int = 0,
 )
 
 
